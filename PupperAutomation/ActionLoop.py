688c1962ffbf78f9e6ee8e965446a825e05bb929
MekaNekodemo <MartinTeitBuchwald@gmail.com>
2021-09-06 13:45:08 +0100

    Queue amputated!

--- PupperAutomation/ActionLoop.py
+++ PupperAutomation/ActionLoop.py
@@ -14,65 +14,65 @@ class ActionLoop:
 
         self.injecter_Connection = InjectionConnection
 
-        self.actionQueue = Queue()
+        # self.actionQueue = Queue()
 
         
     def start(self):
         """
             When called starts sending one message at a time from the actionQueue to the other end of the Multiprocessing pipe connection.
         """
-
         while True:
-
+            
             ## Check for injections
-            if self.injecter_Connection.poll() == True:
-                message: ActionMessage = self.injecter_Connection.recv()
+            # if self.injecter_Connection.poll() == True:
+            #     message: ActionMessage = self.injecter_Connection.recv()
 
-                if message.interrupt == True:
-                    self._handleInterruption(message)
-                else:
-                    self._handleInjection(message)
+                # if message.interrupt == True:
+                #     self._handleInterruption(message)
+                # else:
+                #     self._handleInjection(message)
 
             ## check queue
-            if self.actionQueue.qsize() != 0:
+            # if self.actionQueue.qsize() != 0:
 
                 ## prepare message duration and sending
-                currentActionMsg: ActionMessage = self.actionQueue.get()
+            if self.injecter_Connection.poll() == True:
+                currentActionMsg: ActionMessage = self.injecter_Connection.recv()
                 ticks = currentActionMsg.ticks
                 messageDone = False
 
                 while messageDone == False:
 
-                    ## Check for interrupting messages
+                        ## Check for interrupting messages
                     if self.injecter_Connection.poll() == True:
                         message: ActionMessage = self.injecter_Connection.recv()
 
                         if message.interrupt == True:
-                            ## if message is an interrupt kill current queue, and build new with interruption message
-                            self._handleInterruption(message)
+                                ## if message is an interrupt kill current queue, and build new with interruption message
+                                # self._handleInterruption(message)
                             ticks == 0
                             messageDone = True
                             break
-                        else:
-                            self._handleInjection(message)
+                            # else:
+                                # self._handleInjection(message)
 
                     self.robot_Connection.send(currentActionMsg.toDictionary())
 
                     ticks -= 1
 
                     if ticks <= 0:
-                        messageDone = True
+                            messageDone = True
 
             self.robot_Connection.send(ActionMessage().toDictionary())
 
 
 
-    def _handleInterruption(self, message: ActionMessage ):
-        print("Intterrupt read, interrupting...")
-        self.actionQueue = Queue()
-        self.actionQueue.put(message)
+    # def _handleInterruption(self, message: ActionMessage ):
+    #     print("Intterrupt read, interrupting...")
+    #     self.actionQueue = Queue()
+    #     self.actionQueue.put(message)
 
         
-    def _handleInjection(self, message: ActionMessage):
-        # print("Injection read and message queued")
-        self.actionQueue.put(message)
+    # def _handleInjection(self, message: ActionMessage):
+    #     # print("Injection read and message queued")
+    #     self.actionQueue.put(message)
---
+++ PupperAutomation/BehaviourScripts.py
@@ -0,0 +1,87 @@
+from .behaviorFunctions import *
+import time
+from .openmvAprilTest import *
+import random
+from .HuskyLens import *
+class BehaviourScripts:
+        def randomActionLoop(self):
+            # remember to uncomment sensor code if using infrared temp!
+            #self.connection.send(msg_Trot(interrupt=False))
+            state = 1
+            temp = 0
+            ran = 1
+            while 1:
+                # temp=sensorGetTemp()
+                # print(temp)
+                if temp >= 37:
+                    if state == 1:
+                        self.connection.send(msg_Height_Decrease(70))
+                        time.sleep(0.7)
+                        state = 0
+                    self.connection.send(msg_Wait(500))
+                    self.connection.send(msg_Yaw_Left(30))
+                    self.connection.send(msg_Wait(90))
+                    self.connection.send(msg_Yaw_Right(30))
+                    self.connection.send(msg_Wait(30))
+                    time.sleep(6.8)
+
+                else:
+                    if state == 0:
+                        self.connection.send(msg_Height_Increase(70))
+                        time.sleep(0.7)
+                        state = 1
+                    ran = random.randint(1, 7)
+                    #ran=1
+                    if(ran == 1):
+                        self.connection.send(msg_Trot(interrupt=False))
+                        self.connection.send(msg_Pitch_Up(90))
+                        self.connection.send(msg_Pitch_Down(90))
+                        self.connection.send(msg_Trot(interrupt=False))
+                        time.sleep(1.8)
+
+                    elif(ran == 2):
+                        self.connection.send(msg_Trot(interrupt=False))
+                        self.connection.send(msg_Turn_Left(300))
+                        self.connection.send(msg_Trot(interrupt=False))
+                        time.sleep(3)
+
+                    elif(ran == 3):
+                        self.connection.send(msg_Trot(interrupt=False))
+                        self.connection.send(msg_Strafe_Right(100))
+                        self.connection.send(msg_Strafe_Left(100))
+                        self.connection.send(msg_Trot(interrupt=False))
+                        time.sleep(2)
+
+                    elif(ran == 4):
+                        self.connection.send(msg_Trot(interrupt=False))
+                        self.connection.send(msg_Backwards(120))
+                        self.connection.send(msg_Trot(interrupt=False))
+                        time.sleep(1.2)
+
+                    # elif(ran==5):
+
+                        # self.connection.send(msg_Hop(30))
+                        # self.connection.send(msg_Hop(50))
+                        # time.sleep(0.8)
+
+                    elif(ran == 6):
+                        self.connection.send(msg_Trot(interrupt=False))
+                        self.connection.send(msg_Pitch_Up(100))
+                        self.connection.send(msg_Yaw_Right(40))
+                        self.connection.send(msg_Pitch_Down(100))
+                        self.connection.send(msg_Trot(interrupt=False))
+                        time.sleep(2.4)
+
+                    elif(ran == 7):
+                        self.connection.send(msg_Height_Decrease(80))
+                        self.connection.send(msg_Pitch_Down(100))
+                        self.connection.send(msg_Height_Increase(80))
+                        time.sleep(2.6)
+
+                self.connection.send(msg_Wait(200))
+            # sensorClose()
+
+
+        def playDead(self):
+            self.connection.send(msg_Height_Decrease(100))
+            self.connection.send(msg_Wait(500))
