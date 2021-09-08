3a4fde14ac3d8235cfb1878d13379c353baf93f3
RocketBadger <merrildt@gmail.com>
2021-09-08 14:41:16 +0100

    LIDAR mysteriously working again suspect loose connection or fuckery

--- sensors/RPLIDAR/RplidarTest.py
+++ sensors/RPLIDAR/RplidarTest.py
@@ -2,11 +2,11 @@ from rplidar import RPLidar
 port = '/dev/ttyUSB_RPLIDAR'
 lidar = RPLidar(port)
 
-info = lidar.get_info()
-print(info)
+# info = lidar.get_info()
+# print(info)
 
-health = lidar.get_health()
-print(health)
+# health = lidar.get_health()
+# print(health)
 
 for i, scan in enumerate(lidar.iter_scans()):
     print('%d: Got %d measurements' % (i, len(scan)))
